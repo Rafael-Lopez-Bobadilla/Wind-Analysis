9bd5461f04c28d1d33d76b24b6b028c7b9f04bc3
Rafael Lopez Bobdailla <rafa_lb5@hotmail.com>
2024-02-16 15:03:51 +0100

    first commit

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <Head>
+    <Title>Wind Analysis</Title>
+  </Head>
+  <div class="m-[50px]">
+    Hola
+  </div>
+</template>
+
+<style>
+body {
+  background-color: #f1f1f1;
+}
+</style>
